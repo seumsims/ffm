94bb44e0e20b0582ef58e591b3232a4d521ec451
Seum <seumworldinbangladesh@gmail.com>
2021-09-14 04:06:08 +0600

    starts

---
+++ ffm.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Featured Foto Mod
+Plugin URI: https://github.com/seumsims/ffm
+Description: 
+Version: 1.00
+Author: Seum
+Author URI: https://github.com/seumsims/
+License: MIT License
+License URI: https://www.mit.edu/~amini/LICENSE.md
+*/
